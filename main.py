2ecdff8e7a40b9e2653e935bf3fc24746061c562
Yi Xian <exian0716@gmail.com>
2024-04-28 19:56:10 +0800

    Regular code update

--- main.py
+++ main.py
@@ -17,15 +17,21 @@ match L5_selection:
         original_df = pd.read_csv('data/L5SacrumAnteroPosteriorForce.csv', header=None, nrows=3)
         modified_df = pd.read_csv('data/L5SacrumAnteroPosteriorForce.csv', header=None, skiprows=3)
     case '1':
-        original_df = pd.read_csv('data/L5SacrumAnteroLateralForce.csv', header=None, nrows=3)
-        modified_df = pd.read_csv('data/L5SacrumAnteroLateralForce.csv', header=None, skiprows=3)
+        original_df = pd.read_csv('data/L5SacrumMedioLateralForce.csv', header=None, nrows=3)
+        modified_df = pd.read_csv('data/L5SacrumMedioLateralForce.csv', header=None, skiprows=3)
     case '2':
-        original_df = pd.read_csv('data/L5SacrumAnteroDistalForce.csv', header=None, nrows=3)
-        modified_df = pd.read_csv('data/L5SacrumAnteroDistalForce.csv', header=None, skiprows=3)
+        original_df = pd.read_csv('data/L5SacrumProximoDistalForce.csv', header=None, nrows=3)
+        modified_df = pd.read_csv('data/L5SacrumProximoDistalForce.csv', header=None, skiprows=3)
 
 # Modify the cloned dataframe
 for i in range(2, len(modified_df)):
-    rand_int = random.randint(-100, 100)
+    match L5_selection:
+        case '0':
+            rand_int = random.randint(-100, 100)
+        case '1':
+            rand_int = random.randint(-20, 20)
+        case '2':
+            rand_int = random.randint(-200, 200)
     
     modified_df.iloc[i, 1] += rand_int
     print(f'Modifying Row [{i}]............ {modified_df.iloc[i, 1]:.10f} ({rand_int})')
@@ -38,8 +44,8 @@ match L5_selection:
     case '0':
         modified_df.to_csv('modified_data/Modified_L5SacrumAnteroPosteriorForce.csv', index=False, header=False)
     case '1':
-        modified_df.to_csv('modified_data/Modified_L5SacrumAnteroLateralForce.csv', index=False, header=False)
+        modified_df.to_csv('modified_data/Modified_L5SacrumMedioLateralForce.csv', index=False, header=False)
     case '2':
-        modified_df.to_csv('modified_data/Modified_L5SacrumAnteroDistalForce.csv', index=False, header=False)
+        modified_df.to_csv('modified_data/Modified_L5SacrumProximoDistalForce.csv', index=False, header=False)
 
 logging.info('Modified dataFrame saved to CSV successfully')
