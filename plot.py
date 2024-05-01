f13ef9af5b8150967eebe9066b91055182eee020
Yi Xian <exian0716@gmail.com>
2024-05-02 04:45:36 +0800

    Regular code update

--- plot.py
+++ plot.py
@@ -2,16 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read the CSV file, skipping the first 3 rows
-csv_dir = 'data/L5SacrumProximoDistalForce.csv'
+csv_dir = 'fig\S4\S4_CpstKneeRot\L5SacrumProximoDistalForce.csv'
+save_dir = 'fig\S4\S4_CpstKneeRot\L5SacrumProximoDistalForce.png'
 df = pd.read_csv(csv_dir, skiprows=3)
 
 x = df.iloc[:, 0]
 y = df.iloc[:, 1]
 
 # Plot the data
-plt.plot(x, y)
-plt.xlabel('Time, t (s)')
-plt.ylabel('Joint Reaction Force, F (N)')
+plt.figure(figsize=(8, 6))
+plt.plot(x, y, linewidth=1.1)
+plt.xlabel('Time, t (s)', fontsize=12)
+plt.ylabel('Joint Reaction Force, F (N)', fontsize=12)
 
 if 'AnteroPosterior' in csv_dir:
     plt.title('L5 Sacrum Antero Posterior Force')
@@ -20,5 +22,6 @@ elif 'MedioLateral' in csv_dir:
 else:
     plt.title('L5 Sacrum Proximo Distal Force')
 
-plt.grid(True)
+#plt.grid(True)
+plt.savefig(save_dir)
 plt.show()
---
+++ test.py
@@ -0,0 +1,67 @@
+import os
+import logging
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(levelname)s:: %(asctime)s - %(message)s')
+
+base_dir = 'fig'
+
+def plot_csv_file(folder_name, csv_file_path, png_file_path):
+    """Plot the data from a CSV file and save the plot."""
+    
+    # Read the CSV file, skipping the first 3 rows
+    df = pd.read_csv(csv_file_path, skiprows=3)
+
+    x = df.iloc[:, 0]
+    y = df.iloc[:, 1]
+
+    # Plot the data
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, y, linewidth=1.3)
+    plt.xlabel('Time, t (s)', fontsize=12)
+    plt.ylabel('Joint Reaction Force, F (N)', fontsize=12)
+
+    if 'AnteroPosterior' in csv_file_path:
+        plt.title(f'{folder_name} - L5 Sacrum Antero Posterior Force')
+    elif 'MedioLateral' in csv_file_path:
+        plt.title(f'{folder_name} - L5 Sacrum Medio Lateral Force')
+    else:
+        plt.title(f'{folder_name} - L5 Sacrum Proximo Distal Force')
+
+    # plt.grid(True)
+    plt.savefig(png_file_path)
+    plt.close()
+
+def main():
+    """Main program to loop files through all subdirectories."""
+    iter = 0
+    # Loop through 1st Layer subdirectory
+    for subdir1 in os.listdir(base_dir):
+        subdir1 = os.path.join(base_dir, subdir1)
+        
+        # Loop through 2nd Layer subdirectory
+        for subdir2_folder_name in os.listdir(subdir1):
+            subdir2 = os.path.join(subdir1, subdir2_folder_name)
+            
+            # Loop through every files in the subdirectory
+            for file_path in os.listdir(subdir2):
+                # List only csv files
+                if file_path.endswith('.csv'):
+                    
+                    # Initialise the 
+                    csv_dir = os.path.join(subdir2, file_path)
+                    save_dir = csv_dir.replace('csv','png')
+                    plot_csv_file(subdir2_folder_name, csv_dir, save_dir)
+                    iter += 1
+                    logging.info(f"Saving to '{save_dir}' ...... Completed!")
+    
+    print('\n=================================================================================================================================')
+    logging.info(f'{iter} graphs generated successfully!')
+    print('=================================================================================================================================\n')
+
+
+if __name__ == '__main__':
+    main()
