c8326068d17295267d49122ffe79792a9a32b1a7
Yi Xian <exian0716@gmail.com>
2024-05-22 22:39:04 +0800

    Regular code update

--- batch_reba_plot.py
+++ batch_reba_plot.py
@@ -22,7 +22,7 @@ def plot_csv_file(csv_file_path, png_file_path):
     plt.figure(figsize=(8, 6))
     plt.plot(x, y, linewidth=1.3)
     plt.xlabel('Time, t (s)', fontsize=12)
-    plt.ylabel('Joint Reaction Force, F (N)', fontsize=12)
+    plt.ylabel('REBA Score', fontsize=12)
 
     # plt.grid(True)
     plt.savefig(png_file_path)
@@ -33,20 +33,21 @@ def main():
     
     iter = 0
     # Loop through S5_data folder
-    for subdir1 in os.listdir(base_dir):
-        subdir1 = os.path.join(base_dir, subdir1)
+    for subfolder in os.listdir(base_dir):
+        subdir = os.path.join(base_dir, subfolder)
         
         # Loop through every files in the subdirectory
-        for file_path in os.listdir(subdir1):
+        for file_path in os.listdir(subdir):
             # List only csv files
             if file_path.endswith('.csv'):
-                csv_dir = os.path.join(subdir1, file_path)
-                
+                csv_dir = os.path.join(subdir, file_path)
+                # Check reba.csv in each directory
                 if 'reba' in csv_dir.lower():
-                    save_dir = csv_dir.replace('csv','png')
+                    reba_img = f'{subfolder}.png'
+                    save_dir = os.path.join('reba_score', reba_img)
                     plot_csv_file(csv_dir, save_dir)
                     iter += 1
-                    #logging.info(f"Saving to '{save_dir}' ...... Completed!")
+                    logging.info(f"Saving to '{save_dir}' ...... Completed!")
     
     print('\n=================================================================================================================================')
     logging.info(f'{iter} graphs generated successfully!')
